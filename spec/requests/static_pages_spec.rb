c1cf6bf7a1eb540b9bb45430e87f552a3181b313
Yoshiki Aoki <ringohub@gmail.com>
2014-03-23 17:51:38 +0900

    Add page title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe "Static pages" do
       visit '/static_page/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the right title" do
+      visit '/static_page/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 
   describe "Help Page" do
@@ -14,6 +19,22 @@ describe "Static pages" do
       visit '/static_page/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the right title" do
+      visit '/static_page/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
   end
 
+  describe "About Page" do
+    it "should have the content 'About Us'" do
+      visit '/static_page/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_page/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+    end
+  end
 end
